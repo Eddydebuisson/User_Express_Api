f593968e1d1e321d62846dee5c4012bc301bbde8
eddy debuisson <eddydebuisson@gmail.com>
2023-01-06 11:59:12 +0100

    Feat: ajout du model user :sparkles:

---
+++ src/model/user.model.ts
@@ -0,0 +1,14 @@
+export default class User {
+    id!:number
+    nom: string
+    prenom: string
+    date_de_naissance?: string = ""
+    date_inscription: Date
+    nationalite?: string = ""
+
+    constructor(nom:string, prenom:string,date_inscription: Date){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.date_inscription = date_inscription;
+    }
+}
